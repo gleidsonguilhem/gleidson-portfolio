9c999de50893ccae8171780ee316b7e75a16d299
Gleidson Guilhem <gleidson.guilhem@gmail.com>
2025-06-10 11:56:03 +1000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Hello, I'm Gleidson</h1>
+      <p>Full-Stack Developer | Web Enthusiast</p>
+      <a href="mailto:gleidson.guilhem@gmail.com">Contact Me</a>
+    </div>
+  );
+};
+
+export default App;
